f26e547a0da5470abf612a986cb4c442da5fce49
EnzoNli <nulli.enzo@hotmail.com>
2022-03-15 22:46:00 +0100

    test

---
+++ insertion.sql
@@ -0,0 +1,64 @@
+insert into CategorieNourriture
+values 
+    ('o',"omnivore : mange des aliments d'origines végétale et animale"),
+    ('h',"herbivore : se nourrit d'herbes et de plantes basses"),
+    ('c',"carnivore : son régime alimentaire est principalement basé sur la consommation de chairs ou de tissus d'animaux vivants ou morts"),
+    ('i',"insectivore : se nourrit d'insectes ou d'autres arthropodes."),
+    ('p',"piscivore : se nourrit de poissons.")
+;
+
+insert into TypeEnclos
+values
+    ('aq', "Un aquarium est un réservoir rempli d'eau dans lequel vivent des animaux et/ou des plantes aquatiques"),
+    ('cg', "Une cage est un contenant ajouré, le plus souvent grillagé ou à barreaux, destiné à contenir un animal."),
+    ('vl', "Une volière est un enclos assez vaste ordinairement grillagé, généralement une grande cage, où l'on conserve, élève et nourrit des oiseaux d'ornement."),
+    ('tr', "Un terrarium est un milieu confiné imitant le biotope de certaines espèces animales et/ou végétales. Il est l'équivalent d'un aquarium dont l'eau serait remplacée par un substrat de quelques centimètres d'épaisseur disposé sur le fond.")
+;
+
+insert into Espece
+values
+    ("Panda geant", 0, "20 à 25 ans", "70 à 120 kg", 3, 2, "Forets de bambous", 'h'),
+    ("Herisson du désert", 0, "3 à 5 ans", "280 à 510 g", 1, 0, "Deserts", 'i'),
+    ("Girafe", 0, "10 à 15 ans", "550 à 1 200 kg", 0, 2, "Savanes", 'h'),
+    ("Lion de mer de Steller", "25 ans", "300 à 1 100 kg", 1, 1, "Nord de l'océan Pacifique", 'p'),
+    
+
+
+
+
+
+insert into Nourriture 
+values
+    ('Boeuf'),
+    ('Bambou'),
+    ('Branchages'),
+    ('Volaille'),
+    ('Pomme'),
+    ('Poire'),
+    ('Carotte'),
+    ('Ecorces'),
+    ('Herbes'),
+
+    ('Insectes'),
+    ('Sardines'),
+    ('Maquereaux'),
+;
+
+insert into Convenir --inverser avec int clef primaire
+values 
+    ('Boeuf','c'),('Boeuf','o'),
+    ('Volaille','c'),('Volaille','o'),
+
+    ('Bambou','h'),('Bambou','o'),
+    ('Branchages','h'),('Branchages','o'),
+    ('Pomme','h'),('Pomme','o'),
+    ('Poire','h'),('Poire','o'),
+    ('Carotte','h'),('Carotte','o'),
+    ('Ecorces','h'),('Ecorces','o'),
+    ('Herbes','h'),('Herbes','o');
+
+
+insert into Enclos
+values 
+    (0, 5, 6, 'aq', 4),
+    (0, 3, 8, 'aq', 4),
---
+++ main.sql
@@ -0,0 +1,171 @@
+pragma foreign_keys = true;
+
+create table CategorieNourriture( 
+	id_categorie	char(1)		primary key,
+	description_cat	varchar(50)
+);
+
+create table Espece(
+	race	varchar(30)	primary key,
+	nb_dans_zoo	int		check(nb_dans_zoo>=0),
+	espe_vie	varchar(30)		not null,
+	espe_poids_adulte	varchar(30)		not null,
+	dangerosite	int		check(0<=dangerosite and dangerosite<=5)	not null,
+	menace_extinction	int		check(0<=menace_extinction and menace_extinction<=5)	not null,
+	habitat_nat		varchar(30),
+	id_categorie	char(1)		references CategorieNourriture(id_categorie)
+);
+
+create table Animal(
+	nom		varchar(15)		primary key,
+	date_naissance_anim		date  	not null,
+	genre	varchar(7)	check(genre in ('Male','Femelle'))  not null,
+	poids	float		check(poids>0)	not null,
+	origine	varchar(30),
+	race 	varchar(30)		references Espece(race)
+);
+
+create table Nourriture(
+	id_plat		int		primary key		auto_increment,
+	description_plat		varchar(50)
+);
+
+create table TypeEnclos(
+	id_type_enclos		char(2)		primary key,
+	description_type	varchar(50)
+);
+	
+create table Soigneur(
+	id_soign	int		primary key		auto_increment,
+	date_naissance_soign	date,	--check avant aujourdhui moins 18 ans ?
+	nom_soign	 	varchar(30)		not null,
+	prenom_soign	varchar(30)		not null,
+	sexe_soign		char(1)			check(sexe_soign in ('M', 'F'))	
+);
+
+create table Gardien(
+	id_gard		int		primary key		auto_increment,
+	date_naissance_gard		date,	--check avant aujourdhui moins 18 ans ?
+	nom_gard	 	varchar(30)		not null,
+	prenom_gard		varchar(30)		not null,
+	sexe_gard		char(1)			check(sexe_gard in ('M', 'F'))	
+);
+
+create table Enclos(
+	id_enclos	int		primary key		auto_increment,
+	nb_actuel	int 	check(nb_actuel >= 0 and nb_actuel<=nb_max),
+	nb_max		int		check(nb_max>0),
+	taille		int		not null,
+	id_type_enclos		char(2)		references TypeEnclos(id_type_enclos),
+	id_gard		int		references Gardien(id_gard)
+);
+
+create table Animation(
+	id_anim		int		primary key		auto_increment,
+	duree		int		check(duree > 20)	not null,
+	description_anim	varchar(50),
+	id_soign	int		references Soignant(id_soign)
+);
+
+-- coucou, associations 
+
+create table AvoirParent(
+	parent		varchar(15)		references Animal(nom),
+	enfant		varchar(15)		references Animal(nom),
+	constraint 	pkAvoirParent	primary key (parent, enfant)
+);
+
+create table PouvoirCohabiter(
+	race_une	varchar(30)		references Espece(race),
+	race_deux	varchar(30)		references Espece(race),
+	constraint	pkPouvoirCohabiter	primary key (race_une, race_deux)
+);
+
+create table Convenir(
+	id_plat		int 	references Nourriture(id_plat),
+	id_categorie	char(1)		references CategorieNourriture(id_categorie),
+	constraint 	pkConvenir	primary key (id_plat, id_categorie)
+);
+
+create table SeNourrirDe(
+	nom 	varchar(15)		references Animal(nom),
+	id_plat 	int		references Nourriture(id_plat),
+	constraint 	pkSeNourrirDe	primary key (nom, id_plat)
+);
+
+create table Soigner(
+	id_soign 	int		references Soigneur(id_soign),
+	nom 	varchar(15)		references Animal(nom),
+	constraint	pkSoigner	primary key (id_soign, nom)
+);
+
+create table Nourrir(
+	id_soign 	int		references Soigneur(id_soign),
+	nom 	varchar(15)		references Animal(nom),
+	quantite	float 	not null,
+	heure_nourr	time,		--check ??
+	constraint	pkNourrir	primary key (id_soign, nom)
+);
+
+create table PouvoirVivre(
+	race	varchar(30)		references Espece(race),
+	id_type_enclos	char(2)	references TypeEnclos(id_type_enclos),
+	constraint	pkPouvoirVivre	primary key (race, id_type_enclos)
+);
+
+create table Occuper(
+	nom 	varchar(15)		references Animal(nom),
+	id_enclos 	int		references Enclos(id_enclos),
+	date_debut	date	not null, --check?
+	date_fin	date, --check??
+	constraint	pkOccuper	primary key (nom, id_enclos)
+);
+
+create table AvoirLieu(
+	id_anim 	int 	references Animation(id_anim),
+	id_enclos 	int 	references Enclos(id_enclos),
+	date_anim	date, --check?
+	heure_anim	time, --check??
+	constraint	pkAvoirLieu	primary key (id_anim, id_enclos)
+);
+
+-- view
+create view AnimauxDansAquarium as(
+	select nom
+	from TypeEnclos natural join Espece natural join Occuper natural join Animal
+	where id_type_enclos = 'aq'
+);
+
+create view lesOmnivores as(
+	select nom
+	from Animal natural join Espece natural join CategorieNourriture
+	where id_categorie = 'o'
+);
+
+create view EnclosVide as(
+	select id_enclos
+	from Enclos
+	where nb_actuel = 0
+);
+
+create view EnclosPlein as(
+	select id_enclos
+	from Enclos
+	where nb_actuel = nb_max
+);
+
+create view RacesQuiCohabitentActuellement as(
+	select distinct r1.race, r2.race
+	from (Espece natural join Animal natural join Occuper natural join Enclos) as r1, (Espece natural join Animal natural join Occuper natural join Enclos) as r2
+	where r1.nom < r2.nom and r1.enclos = r2.enclos
+);
+
+create view QuiPeutMangerQuoi as (
+	select distinct race as qui, description_plat as quoi
+	from (Espece natural join Appartenir natural join Categorie natural join Convenir natural join Nourriture)
+	where 
+)
+
+create view QuiMangeVraimentQuoi as (
+	select 
+)
