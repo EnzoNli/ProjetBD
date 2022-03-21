6a1581887a985713534fba41c239534282cdf94e
Enzo <101291649+EnzoNli@users.noreply.github.com>
2022-03-21 22:13:35 +0100

    Add files via upload

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,26 +1,29 @@
-<nav class="navbar navbar-expand-sm bg-light navbar-light">
-   <a class="navbar-brand" href="#">
-   <img src="img/dino_logo.jpg" style="width:64px;">
+<link rel="stylesheet" href="../css/navbar.css">
+
+<nav class="navbar navbar-expand-sm">
+   <a class="navbar-brand" href="/index.php">
+    <p id="titre">Zouzoo</p>
    </a>
+   
+      <div class="menugauche">
+         <ul class="navbar-nav">
+            <li class="nav-item hover">
+               <a class="nav-link" href="/pages/animaux.php">Animaux</a>
+            </li>
+            <li class="nav-item hover">
+               <a class="nav-link" href="#">Animations</a>
+            </li>
+            <li class="nav-item hover last">
+               <a class="nav-link" href="#">Soigneurs</a>
+            </li>
+         </ul>
+      </div>
 
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-         <a class="nav-link active" href="#">Especes</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" href="#">Enclos</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" href="#">Animations</a>
-      </li>
-    </ul>
-
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Espace administrateur</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
+    <div class="dropdown ml-auto hover">
+      <a class="dropdown-toggle nav-link" href="#" data-bs-toggle="dropdown">Espace administrateur</a>
+      <div class="dropdown-menu">
+        <a href="#" class="dropdown-item">Action</a>
+        <a href="#" class="dropdown-item">Another action</a>
+    </div>
+   </div>
 </nav>
--- index.php
+++ index.php
@@ -2,35 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Zoo</title>
+    <title>Zouzoo - Accueil</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/flickity.css" media="screen">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
     <?php
     include_once("includes/navbar.php");
-    /*
-    unlink("zoo.db");
+    if(file_exists("zoo.db")){
+        unlink("zoo.db");
+    }
+    
     $compteur = 0;
     
     $db = new SQLite3("zoo.db");
     $creation = file_get_contents("main.sql");
     $db->exec($creation);
-    $q = $db->query("SELECT * FROM animal");
-    while($ligne = $q->fetchArray()){
-        echo $ligne['nom'] . " ";
-        $compteur++;
-    }
-    */
+    $q = $db->query("SELECT * FROM nombretotalanimauxzoo");
+    $ligne = $q->fetchArray();
     
     ?>
+    <script src="js/flickity.pkgd.min.js"></script>
 
-    <div class="jumbotron banniere">
-        <h1 class="display-2">Test</h1>
-        <p class="lead">Bienvenue dans notre Zoo ! Il y a actuellement <?php echo $compteur ?> animaux dans le zoo</p>
-        <img class="panda-banniere" src="img/panda.png">
+    <div class="main-gallery js-flickity"
+  data-flickity-options='{ "cellAlign": "left", "contain": true, "pageDots": false , "wrapAround": true, 
+    "autoPlay": 5000, "pauseAutoPlayOnHover": false, "prevNextButtons": false, "selectedAttraction": 0.01, "friction": 0.30, "draggable": false }'>
+        <div class="gallery-cell">
+            <hr class="ligne">
+            <p class="msg">BIENVENUE CHEZ ZOUZOO</p>
+            <img class="banniere" src="img/banniere.jpg" alt="Banniere">
+        </div>
+        <div class="gallery-cell">
+            <hr class="ligne" id="ligne2">
+            <p class="msg" id="msg2">IL Y A ACTUELLEMENT<br><?php echo $ligne[0] ?> ANIMAUX<br>DANS LE ZOO</p>
+            <img class="banniere" src="img/banniere2.jpg" alt="Banniere">
+        </div>
     </div>
+
     
 </body>
 </html>
